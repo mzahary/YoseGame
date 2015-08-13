83745919ebe456f043b145aea4889e9b4531fe33
mzahary <mohamad.zahary@gmail.com>
2015-08-13 23:14:33 +0800

    asd

--- src/main/java/yose/PowerOfTwo.java
+++ src/main/java/yose/PowerOfTwo.java
@@ -5,46 +5,46 @@ import static com.vtence.molecule.http.MimeTypes.JSON;
 import com.google.gson.Gson;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class PowerOfTwo {
+
     private final Gson gson;
 
-	double prime = 0;
-	double square = 0;
-	
-	public PowerOfTwo(Gson gson){
-//		Math.pow(a, b);
-		this.gson = gson;
-	}
-	
-	 public void prime(Request request, Response response) throws Exception {
-		String number = request.parameter("number");
-		StringBuilder result = new StringBuilder();
-                result.append("\"number\":"+number);
-                result.append(",");
-                result.append("\"decomposition\"");
-                result.append(":[");
-		if(number!=null){
-			int prime = Integer.parseInt(number);
-			int i = 0;
-		
-			
-			while(prime!=1){
-				
-				result.append("2");
-				prime = prime/2;
-				
-				if(prime!=1){
-					result.append(",");
-				}
-			}
-		}
-                result.append("]");
-        response.contentType(JSON).body(gson.toJson(result));
+    private int number;
+    private List<Integer> decomposition;
+
+    public PowerOfTwo(Gson gson) {
+        this.gson = gson;
     }
-	
-	public static class Prime {
+
+    public void prime(Request request, Response response) throws Exception {
+//        String number = request.parameter("number");
+
+        if (request.parameter("number") != null) {
+            number = Integer.parseInt(request.parameter("number"));
+            double prime = number;
+            decomposition = new ArrayList<Integer>();
+            int i = 0;
+
+            while (prime != 1) {
+                decomposition.add(2);
+                prime = prime / 2;
+
+            }
+        }
+        response.contentType(JSON).body(gson.toJson(new Prime(number, decomposition)));
+    }
+
+    public static class Prime {
+        private int number;
+        private List<Integer> decomposition;
+        
+        public Prime(int number, List<Integer> decomposition){
+            this.number = number;
+            this.decomposition = decomposition;
+        }
 //        public final boolean alive = true;
     }
 }
